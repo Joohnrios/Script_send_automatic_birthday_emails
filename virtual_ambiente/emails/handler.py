895021d8f6e12dda7d4e699b6d24d97999f71347
John Rios <71901224+Joohnrios@users.noreply.github.com>
2022-11-21 19:17:42 -0300

    Updating to English version... (1.0)
    I'm changing the language for greater reach and help to the community.

--- virtual_ambiente/emails/handler.py
+++ virtual_ambiente/emails/handler.py
@@ -3,25 +3,25 @@ import email.message
 
 
 # Função para enviar o email...
-def enviar_email(name, email_usuario):
+def send_email(name, email_user):
     corpo_email = f"""
-    <p> Salve meu/minha little bro!</p>
-    <p> Feliz aniversário {name}, tudo de bom e de melhor para você e sua familia, você é barril ao ²! ;)</p>
-    <p> É NOIX! <3</p>
+    <p> Hello my little bro!</p>
+    <p> Congratulations {name}, all the best to you and your family...</p>
+    <p> <3 <3 </p>
     """
 
     msg = email.message.Message()
-    msg['Subject'] = "Parabéns!!!"
-    msg['From'] = 'joohnsrios@gmail.com'
-    msg['To'] = email_usuario
-    password = 'fcvzswbtezdhhstj'  # senha do from = remetente
+    msg['Subject'] = "Congratulations!!!"
+    msg['From'] = 'your_email@...'
+    msg['To'] = email_usuer
+    password = 'fcvzswbtezdhhstj'  # senha do from = remetente # from password
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email)
 
     s = smtplib.SMTP('smtp.gmail.com: 587')
     s.starttls()
-    # Login com as credenciais para enviar o email.
+    # Login com as credenciais para enviar o email. # Login with the credentials to send the email.
     s.login(msg['From'], password)
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-    print('Enviado com sucesso!')
+    print('Sent with success!')
 
